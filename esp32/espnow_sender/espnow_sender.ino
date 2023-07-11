3ff1b1241cee8f30f6b3ce9a0f40ee985829f557
OddlyTimbot <timbot@oddlystudios.com>
2023-07-11 11:21:45 -0400

    adding sender

---
+++ esp32/espnow_sender/espnow_sender.ino
@@ -0,0 +1,96 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com  
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "WiFi.h"
+#include <esp_now.h>
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// MAC Address of responder - edit as required
+uint8_t broadcastAddress[] = {0x78, 0x21, 0x84, 0xA0, 0x17, 0x90};
+
+// Callback function called when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+// Define a data structure
+typedef struct struct_message {
+  char a[32];
+  float b;
+  float c;
+} struct_message;
+
+// Create a structured object
+struct_message myData;
+
+// Peer info
+esp_now_peer_info_t peerInfo;
+
+void setup() {
+  // Start the Serial Monitor
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+
+  // Initilize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+ 
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Print MAC Address to Serial monitor
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+  // Start the DS18B20 sensor
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.print(temperatureF);
+  Serial.println("ºF");
+
+  //send to peer
+  // Format structured data
+  strcpy(myData.a, "Temperatures C and F!");
+  myData.b = temperatureC;
+  myData.c = temperatureF;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sending confirmed");
+  }
+  else {
+    Serial.println("Sending error");
+  }
+
+  delay(5000);
+}
